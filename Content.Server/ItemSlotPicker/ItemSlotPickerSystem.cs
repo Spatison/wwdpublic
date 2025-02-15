8150c1c82a030beac2337404a1a0cca264cc6a6f
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2025-02-16 02:25:07 +0300

    initial sidestream port

---
+++ Content.Server/ItemSlotPicker/ItemSlotPickerSystem.cs
@@ -0,0 +1,10 @@
+using Content.Shared.ItemSlotPicker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Server.ItemSlotPicker;
+
+public sealed class ItemSlotPickerSystem : SharedItemSlotPickerSystem;
